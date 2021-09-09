ac0d880c4c435a75df5f1567b269d43fb3c53e4d
gpdoud <gpdoud@gmail.com>
2021-09-09 08:37:42 -0400

    Initial commit

---
+++ KenMvcListWithPaging/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KenMvcListWithPaging.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KenMvcListWithPaging.Data {
+
+    public class AppDbContext : DbContext {
+
+        public DbSet<Log> Logs { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
